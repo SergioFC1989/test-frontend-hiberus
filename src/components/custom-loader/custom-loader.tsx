aff46287838ab9fb6443c2ee17666d06d17d7c4d
Sergio <52042600+SergioFC1989@users.noreply.github.com>
2024-07-30 10:55:18 +0200

    [GH-4]: fix: Errors (#7)
    * chore: Initial status of results at 0. added link component to header logo
    
    * feat: created component custom loader
    
    * chore: differentiate the cache and localstorage keys by environment
    
    * chore: removed breakpoint
    
    ---------
    
    Co-authored-by: SergioFC89 <sergio.fernandez@nixi1.com>

---
+++ src/components/custom-loader/custom-loader.tsx
@@ -0,0 +1,3 @@
+import styles from "./custom-loader.module.css";
+
+export const CustomLoader = () => <span className={styles.loader}></span>;
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CustomCardCharacter } from "@/components/custom-card-character/custom-card-character";
+import { CustomLoader } from "@/components/custom-loader/custom-loader";
 import { CustomTextInputSearch } from "@/components/custom-text-input-search/custom-text-input-search";
 import { useFavoriteCharacters } from "@/hooks/useFavoriteCharacters";
 import { useHome } from "@/hooks/useHome";
@@ -17,14 +18,13 @@ export const Home = () => {
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value.toUpperCase())}
         />
-        <p>{filteredValue?.length} RESULTS</p>
+        <p>{filteredValue?.length ?? 0} RESULTS</p>
       </div>
       {isLoading ? (
-        <h3>Loading...</h3>
+        <CustomLoader />
       ) : (
         <div className={styles.characters}>
-          {filteredValue &&
-            filteredValue.length &&
+          {filteredValue && filteredValue.length ? (
             filteredValue.map((character) => {
               const isFav = !!charactersFav?.find(
                 (item) => item.id === character.id
@@ -38,7 +38,10 @@ export const Home = () => {
                   onClick={() => handleSaveCharacterFav(character)}
                 />
               );
-            })}
+            })
+          ) : (
+            <></>
+          )}
         </div>
       )}
     </div>
